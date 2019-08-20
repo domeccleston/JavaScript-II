c3d34a5497e2f6b16ae492260bc5f422b56ba8f6
Dom <domeccleston@gmail.com>
2019-08-20 12:01:33 +0100

    did callbacks.js stretch goal

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -38,7 +38,6 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
   return cb(arr.length);
@@ -46,7 +45,7 @@ function getLength(arr, cb) {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-  return cb(arr[arr.length-1]);
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
@@ -56,7 +55,7 @@ function sumNums(x, y, cb) {
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
-  return cb(x*y);
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
@@ -67,8 +66,17 @@ function contains(item, list, cb) {
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
+// example callback function: add " unique!" to an array member
+let isUnique = item => item + ": unique!";
+
+function removeDuplicates(arr, cb) {
+  let uniquesArr = arr.reduce((unique, item) => {
+    // create an accumulator of unique values
+    return unique.includes(item) ? unique : [...unique, item];
+    // if the value exists in the accumulator, return it
+  }, []);
+  // we have an array of unique values, to which we can map our callback function
+  return uniquesArr.map(cb);
 }
+
+removeDuplicates(items, isUnique);
