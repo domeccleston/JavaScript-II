38d845ca1375d989b4c0979afe01bb3c0b8fcc75
Dom <domeccleston@gmail.com>
2019-08-20 11:21:55 +0100

    finished MVP

--- assignments/closure.js
+++ assignments/closure.js
@@ -3,7 +3,11 @@
 // Keep it simple! Remember a closure is just a function
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
+let globalVariable = 1;
 
+let closureFunction = function() {
+  return 5 + globalVariable;
+}
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
